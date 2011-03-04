e9b2a84b27833388401f2f8d361e7181a097a1e3
Steve Kemp <steve@steve.org.uk>
2011-03-04 18:28:55 +0000

    Stub.  Add the new snapshot utility

---
+++ src/redisfs-snapshot.c
@@ -0,0 +1,9 @@
+/* redisfs-snapshot.c -- Utility to create a filesystem snapshot
+ *
+ *
+ * Copyright (c) 2011, Steve Kemp <steve@steve.org.uk>
+ * All rights reserved.
+ *
+ * http://steve.org.uk/
+ *
+ */
