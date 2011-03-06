66487d4d2ad9cf584043dc3eeffd9cd6cb6b2e4d
Steve Kemp <steve@steve.org.uk>
2011-03-06 00:39:01 +0000

    Removed.  D'oh

--- tests/fastlz_test.c
+++ tests/fastlz_test.c
@@ -37,12 +37,27 @@ TestNOP (CuTest * tc)
 }
 
 
+void
+TestCompress (CuTest * tc)
+{
+  char input[] = { "This is a test input string.  I like to test input." };
+  char output[200];
+  int  input_len = strlen( input );
+
+  int compressdata_size = fastlz_compress_level(2, (void*)input, input_len, output );
+
+  printf("LEN:%d -> %d\n", input_len, compressdata_size);
+  CuAssertTrue (tc, compressdata_size < input_len );
+
+}
+
 CuSuite *
 fastlz_getsuite ()
 {
     CuSuite *suite = CuSuiteNew ();
 
     SUITE_ADD_TEST (suite, TestNOP);
+    SUITE_ADD_TEST (suite, TestCompress);
 
     return suite;
 }
